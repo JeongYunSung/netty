66c3f720b553a239d177c08cc25bc915a77c93e0
김상윤 <probaseball@polycube.co.kr>
2022-10-17 07:47:31 +0900

    feature: echo server

--- src/main/kotlin/com/yunseong/netty/NettyApplication.kt
+++ src/main/kotlin/com/yunseong/netty/NettyApplication.kt
@@ -1,10 +1,12 @@
 package com.yunseong.netty
 
 import io.netty.bootstrap.ServerBootstrap
+import io.netty.buffer.ByteBuf
 import io.netty.channel.*
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioServerSocketChannel
+import java.nio.charset.Charset
 
 fun main(args: Array<String>) {
     val bossGroup: EventLoopGroup = NioEventLoopGroup()
@@ -16,7 +18,7 @@ fun main(args: Array<String>) {
             @Throws(Exception::class)
             override fun initChannel(ch: SocketChannel) {
                 val p = ch.pipeline()
-                p.addLast(DiscardServerHandler())
+                p.addLast(EchoServerHandler())
             }
         })
     val future = b.bind(8888).sync()
@@ -32,5 +34,20 @@ internal class DiscardServerHandler : SimpleChannelInboundHandler<Any?>() {
     override fun exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable) {
         cause.printStackTrace()
         ctx.close()
+    }
+}
+
+internal class EchoServerHandler : ChannelInboundHandlerAdapter() {
+
+    override fun channelRead(ctx: ChannelHandlerContext?, msg: Any?) {
+        var readMessage: String = (msg as ByteBuf).toString(Charset.defaultCharset())
+
+        println("수신한 문자열 [$readMessage]")
+
+        ctx?.write(msg)
+    }
+
+    override fun channelReadComplete(ctx: ChannelHandlerContext?) {
+        ctx?.flush()
     }
 }
