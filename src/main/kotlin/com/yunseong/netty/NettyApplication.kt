997f362ad643d2240ed7618de887c34b8de784b9
김상윤 <probaseball@polycube.co.kr>
2022-10-18 17:58:40 +0900

    feature: default http server

--- src/main/kotlin/com/yunseong/netty/NettyApplication.kt
+++ src/main/kotlin/com/yunseong/netty/NettyApplication.kt
@@ -1,15 +1,18 @@
 package com.yunseong.netty
 
 import io.netty.bootstrap.ServerBootstrap
+import io.netty.bootstrap.ServerBootstrapConfig
 import io.netty.buffer.ByteBuf
+import io.netty.buffer.Unpooled
 import io.netty.channel.*
+import io.netty.channel.local.LocalAddress
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioServerSocketChannel
-import io.netty.handler.codec.http.HttpRequest
-import io.netty.handler.codec.http.HttpServerCodec
+import io.netty.handler.codec.http.*
 import io.netty.handler.logging.LogLevel
 import io.netty.handler.logging.LoggingHandler
+import java.net.InetSocketAddress
 import java.nio.charset.Charset
 
 fun main(args: Array<String>) {
@@ -23,16 +26,16 @@ fun main(args: Array<String>) {
             @Throws(Exception::class)
             override fun initChannel(ch: SocketChannel) {
                 val p = ch.pipeline()
-                p.addLast(LoggingHandler(LogLevel.INFO))
                 p.addLast(HttpServerCodec())
                 p.addLast(HttpServerInboundHandler())
             }
         })
-    val future = b.bind(8888).sync()
+    val future = b.bind(System.getProperty("port").toInt()).sync()
     future.channel().closeFuture().sync()
 }
 
 internal class DiscardServerHandler : SimpleChannelInboundHandler<Any?>() {
+
     @Throws(Exception::class)
     override fun channelRead0(ctx: ChannelHandlerContext, msg: Any?) {
     }
@@ -62,14 +65,28 @@ internal class EchoServerHandler : ChannelInboundHandlerAdapter() {
 internal class HttpServerInboundHandler : ChannelInboundHandlerAdapter() {
 
     override fun channelRead(ctx: ChannelHandlerContext?, msg: Any?) {
-        val readMessage: HttpRequest = msg as HttpRequest
+        if(msg is HttpRequest) {
+            println("request: ${msg.uri()}")
+            if (msg.uri().equals("/health")) {
+                val localAddress: InetSocketAddress = ctx?.channel()?.localAddress() as InetSocketAddress
 
-        println("경로 [${readMessage.uri()}]")
-
-        ctx?.write(msg)
+                ctx.write(getResponse(HttpResponseStatus.OK, Unpooled.wrappedBuffer(localAddress.port.toString().toByteArray())))
+            }else {
+                ctx?.write(getResponse(HttpResponseStatus.NOT_FOUND, Unpooled.wrappedBuffer("".toByteArray())))
+            }
+        }
     }
 
     override fun channelReadComplete(ctx: ChannelHandlerContext?) {
         ctx?.flush()
+    }
+
+    private fun getResponse(status: HttpResponseStatus, content: ByteBuf): FullHttpResponse {
+        val response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, status, content)
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes())
+        response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE)
+
+        return response
     }
 }
