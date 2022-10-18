8d2b05ec2ad061fa9d2518689bf202ce7a07d987
김상윤 <probaseball@polycube.co.kr>
2022-10-18 11:05:32 +0900

    feature: 로깅 핸들러 추가 및 Http Codec추가

--- src/main/kotlin/com/yunseong/netty/NettyApplication.kt
+++ src/main/kotlin/com/yunseong/netty/NettyApplication.kt
@@ -6,6 +6,10 @@ import io.netty.channel.*
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioServerSocketChannel
+import io.netty.handler.codec.http.HttpRequest
+import io.netty.handler.codec.http.HttpServerCodec
+import io.netty.handler.logging.LogLevel
+import io.netty.handler.logging.LoggingHandler
 import java.nio.charset.Charset
 
 fun main(args: Array<String>) {
@@ -14,11 +18,14 @@ fun main(args: Array<String>) {
     val b = ServerBootstrap()
     b.group(bossGroup, workerGroup)
         .channel(NioServerSocketChannel::class.java)
+        .handler(LoggingHandler(LogLevel.INFO))
         .childHandler(object : ChannelInitializer<SocketChannel>() {
             @Throws(Exception::class)
             override fun initChannel(ch: SocketChannel) {
                 val p = ch.pipeline()
-                p.addLast(EchoServerHandler())
+                p.addLast(LoggingHandler(LogLevel.INFO))
+                p.addLast(HttpServerCodec())
+                p.addLast(HttpServerInboundHandler())
             }
         })
     val future = b.bind(8888).sync()
@@ -43,6 +50,21 @@ internal class EchoServerHandler : ChannelInboundHandlerAdapter() {
         val readMessage: String = (msg as ByteBuf).toString(Charset.defaultCharset())
 
         println("수신한 문자열 [$readMessage]")
+
+        ctx?.write(msg)
+    }
+
+    override fun channelReadComplete(ctx: ChannelHandlerContext?) {
+        ctx?.flush()
+    }
+}
+
+internal class HttpServerInboundHandler : ChannelInboundHandlerAdapter() {
+
+    override fun channelRead(ctx: ChannelHandlerContext?, msg: Any?) {
+        val readMessage: HttpRequest = msg as HttpRequest
+
+        println("경로 [${readMessage.uri()}]")
 
         ctx?.write(msg)
     }
